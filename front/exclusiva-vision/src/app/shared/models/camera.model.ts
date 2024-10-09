6a4412f5891b67661a24557be02f121d949df6e0
Guilherme Gentile <uk.gentile@gmail.com>
2024-10-09 17:02:30 -0300

    Added Model

---
+++ front/exclusiva-vision/src/app/shared/models/camera.model.ts
@@ -0,0 +1,59 @@
+export interface Camera {
+    id: string;
+    name: string;
+    description: string;
+    rtspUrl: string;
+    isActive: boolean;
+    lastChecked: Date;
+  
+    // Parâmetros para a configuração de vídeo
+    width?: number;
+    height?: number;
+    framerate?: number;
+    bitrate?: number;
+    analyzeDuration?: string;
+    probesize?: string;
+  
+    // Tunning
+    zeroLatency?: boolean;
+    noBuffer?: boolean;
+    gop?: number;
+    bufferSize?: number;
+    threads?: number;
+    vsync?: boolean;
+    useServerTimestamps?: boolean;
+  
+    // Opção para configuração de codificação de vídeo
+    videoCodec: string;
+    preset: string;
+    crf?: number;
+  
+    // Opções para configuração de áudio
+    audioEnabled: boolean;
+    audioCodec?: string;
+    audioBitrate?: number;
+    audioChannels?: number;
+  
+    // Configurações de HLS
+    hlsTime: number;
+    hlsListSize: number;
+  
+    // Parâmetros de Rede
+    rtspTransport: string;
+  
+    // Parâmetros do Container
+    serviceId?: string;
+    isRunning: boolean;
+    isRequested: boolean;
+    lastRequested?: Date;
+  
+    // Snapshot
+    cameraSnapshotUrl?: string;
+    generateSnapshot: boolean;
+    snapshotFPS: number;
+  
+    // Urls
+    hlsUrl?: string;
+    snapshotUrl?: string;
+  }
+  
