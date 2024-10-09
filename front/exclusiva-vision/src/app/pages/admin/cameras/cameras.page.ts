333508f0a8e2f4cfa73b7a7fe3b7e8252697306e
Guilherme Gentile <uk.gentile@gmail.com>
2024-10-09 17:20:58 -0300

    Added call to api

--- front/exclusiva-vision/src/app/pages/admin/cameras/cameras.page.ts
+++ front/exclusiva-vision/src/app/pages/admin/cameras/cameras.page.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonMenuButton, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonItem, IonList, IonLabel } from '@ionic/angular/standalone';
+import { CameraService } from 'src/app/shared/services/api/camera/camera.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-cameras',
   templateUrl: './cameras.page.html',
   styleUrls: ['./cameras.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonList, IonItem, IonMenuButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class CamerasPage implements OnInit {
+export class CamerasPage {
+  private readonly cameraService = inject(CameraService);
+
+  cameras = toSignal(this.cameraService.getCameras().pipe(map(resp => resp.data)));
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
